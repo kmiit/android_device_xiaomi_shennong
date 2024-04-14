e60882ed0eb39b3d45700361158cbbaa87124157
kmiit <2421768138@qq.com>
2024-04-14 21:00:09 +0800

    shennong: extract-files: Fix XMLs format

--- extract-files.sh
+++ extract-files.sh
@@ -54,6 +54,14 @@ if [ -z "${SRC}" ]; then
     SRC="adb"
 fi
 
+function blob_fixup() {
+    case "${1}" in
+        *.xml)
+            sed -i s/xml=version/xml\ version/g "${2}"
+            ;;
+    esac
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
 
