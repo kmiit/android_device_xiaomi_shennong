b710715289489b4f66c41c514faf717fd3912d33
YuKongA <YuKongA@users.noreply.github.com>
2024-04-14 13:29:00 +0800

    shennong: Import device FCM from stock * from missi_phone_cn_only64-user-14-UKQ1.230804.001-V816.0.37.0.UNBCNXM-release-keys

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -140,5 +140,8 @@ BOARD_AVB_VBMETA_SYSTEM_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 2
 
+# VINTF
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/vintf/compatibility_matrix.device.xml
+
 # Inherit from the proprietary version
 -include vendor/xiaomi/shennong/BoardConfigVendor.mk
