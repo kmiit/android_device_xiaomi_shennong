255f5c429f8315aa9ca6ffc911352dddcd0fd2af
YuKongA <70465933+YuKongA@users.noreply.github.com>
2024-04-14 19:07:12 +0800

    shennong: healthd-ext: Fix C++11 build error

--- healthd-ext/main.cpp
+++ healthd-ext/main.cpp
@@ -55,7 +55,7 @@ ndk::ScopedAStatus HealthImpl::getChargeCounterUah(int32_t* out) {
 static constexpr const char* gInstanceName = "default";
 static constexpr std::string_view gChargerArg{"--charger"};
 
-constexpr char *ucsiPSYName[]{
+constexpr const char *ucsiPSYName[]{
 	"ucsi-source-psy-soc:qcom,pmic_glink:qcom,ucsi1",
 	"ucsi-source-psy-soc:qcom,pmic_glink:qcom,ucsi2"
 };
