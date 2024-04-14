ee6953bf0740a82213782b333cfb65ee012db044
YuKongA <70465933+YuKongA@users.noreply.github.com>
2024-04-14 08:42:35 +0800

    shennong: Add strongbox-nxp & weaver-service.nxp into config.fs
    * Xiaomi 14 series uses them

--- configs/config/config.fs
+++ configs/config/config.fs
@@ -46,6 +46,18 @@ value:2915
 [AID_VENDOR_THALES_WEAVER]
 value:2916
 
+[vendor/bin/hw/android.hardware.security.keymint-service.strongbox-nxp]
+mode: 0755
+user: AID_VENDOR_NXP_STRONGBOX
+group: AID_SYSTEM
+caps: SYS_ADMIN SYS_NICE
+
+[vendor/bin/hw/android.hardware.weaver-service.nxp]
+mode: 0755
+user: AID_VENDOR_NXP_WEAVER
+group: AID_SYSTEM
+caps: SYS_ADMIN SYS_NICE
+
 [vendor/bin/hw/android.hardware.security.keymint-service.strongbox-thales]
 mode: 0755
 user: AID_VENDOR_THALES_STRONGBOX
