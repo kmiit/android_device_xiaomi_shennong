2bac998ea30f771124c41115dc36ced656bad1d6
YuKongA <70465933+YuKongA@users.noreply.github.com>
2024-04-14 08:38:52 +0800

    shennong: Initial properties from stock

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,6 +118,13 @@ TARGET_BOARD_PLATFORM := pineapple
 # Power
 TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
+# Properties
+TARGET_ODM_PROP += $(DEVICE_PATH)/configs/properties/odm.prop
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/properties/product.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/properties/system.prop
+TARGET_SYSTEM_EXT_PROP += $(DEVICE_PATH)/configs/properties/system_ext.prop
+TARGET_VENDOR_PROP += $(DEVICE_PATH)/configs/properties/vendor.prop
+
 # Recovery
 BOARD_EXCLUDE_KERNEL_FROM_RECOVERY_IMAGE := true
 BOARD_USES_RECOVERY_AS_BOOT := false
