481352c8274fc021e81199547b9285c80d64a653
Adithya R <gh0strider.2k18.reborn@gmail.com>
2024-07-16 07:20:38 +0530

    shennong: Override HDR types for dolby vision
    Stock display HAL doesn't enable dolby vision support,
    instead miui uses a custom surfaceflinger lib for the
    same. Since we won't be using that, simply override
    supported HDR types at boot.
    
    Change-Id: I93ecfbd7aef42e5e1a4c696e196e482a023c9ae4

--- device.mk
+++ device.mk
@@ -266,6 +266,10 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
