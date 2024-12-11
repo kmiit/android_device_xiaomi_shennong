4d9293b8267bbadcf6069d8237d262eefa14b509
kmiit <kmiit@foxmail.com>
2024-12-11 22:28:31 +0800

    shennong: Build usb service from source

--- device.mk
+++ device.mk
@@ -425,6 +425,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb-service.qti \
+    android.hardware.usb.gadget-service.qti
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
+
 # Vendor API
 BOARD_SHIPPING_API_LEVEL := 34
 
