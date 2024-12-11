efca58abbae8af191d5ee0816602ca5956dea085
kmiit <kmiit@foxmail.com>
2024-12-11 21:45:38 +0800

    shennong: Remove useless fs libs

--- device.mk
+++ device.mk
@@ -163,12 +163,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
-# F2fs utilities
-PRODUCT_PACKAGES += \
-    sg_write_buffer \
-    f2fs_io \
-    check_f2fs
-
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.xiaomi \
