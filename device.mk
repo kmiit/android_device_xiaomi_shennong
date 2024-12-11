e788be53cfa6fe5b5b8ea1cbbb974c0f03c527cd
kmiit <kmiit@foxmail.com>
2024-12-11 22:27:06 +0800

    shennong: Build thermal from source

--- device.mk
+++ device.mk
@@ -406,7 +406,12 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml \
     frameworks/native/data/etc/android.hardware.telephony.mbms.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.mbms.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
-    
+
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0 \
+    android.hardware.thermal@2.0.vendor
+
 # Update engine
 PRODUCT_PACKAGES += \
     update_engine \
