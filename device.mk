31db2e1f0debc1e2df471d4561d91c7958e34b71
kmiit <kmiit@foxmail.com>
2024-12-11 22:02:03 +0800

    shennong: Copy ipsec_tunnels permission from source

--- device.mk
+++ device.mk
@@ -267,6 +267,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
+# Network
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
