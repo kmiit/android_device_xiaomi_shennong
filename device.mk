bc8f73407349c93170b0b519d3d1f9401189b6cb
kmiit <kmiit@foxmail.com>
2024-12-11 22:25:55 +0800

    shennong: Build service tracker from source

--- device.mk
+++ device.mk
@@ -364,6 +364,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_pineapple/android.hardware.sensor.stepcounter.xml \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/sku_pineapple/android.hardware.sensor.stepdetector.xml
 
+# Servicetracker
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.servicetracker@1.2.vendor
+
 # Shipping API
 PRODUCT_SHIPPING_API_LEVEL := 34
 
