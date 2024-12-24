edce6b815a2225d901866efd85bc7b7c5f17e34a
kmiit <kmiit@foxmail.com>
2024-12-24 21:05:54 +0800

    shennong: Disable adb security for now

--- device.mk
+++ device.mk
@@ -448,8 +448,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service.xiaomi \
-    android.hardware.vibrator-V1-ndk_platform.vendor
+    vendor.qti.hardware.vibrator.service.xiaomi
 
 PRODUCT_COPY_FILES += \
     hardware/xiaomi/aidl/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
