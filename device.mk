1f6622ba42db187ab787946f56f54ef065590364
kmiit <kmiit@foxmail.com>
2024-12-25 11:58:52 +0800

    shennong: telephony: Disable DYNAMIC_RECEIVER_EXPLICIT_EXPORT_REQUIRED for ims packages
    A better workaround for ca677fc392973b6fe0610c045b63387235f33d55
    
    Signed-off-by: kmiit <kmiit@foxmail.com>

--- device.mk
+++ device.mk
@@ -48,6 +48,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_VIRTUAL_AB_COMPRESSION_METHOD := lz4
 
+# App compat
+PRODUCT_PACKAGES += \
+    compat_framework_overrides.xml
+
 # ANT+
 PRODUCT_PACKAGES += \
     com.dsi.ant@1.0.vendor
