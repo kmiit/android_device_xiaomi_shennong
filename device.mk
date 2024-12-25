91f0c0767d4695fcc68e81b5a0918228f8ca7d92
kmiit <kmiit@foxmail.com>
2024-12-25 17:36:29 +0800

    shennong: usb: Build usb_compositions.conf from source

--- device.mk
+++ device.mk
@@ -448,7 +448,8 @@ PRODUCT_PACKAGES_DEBUG += \
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb-service.qti \
-    android.hardware.usb.gadget-service.qti
+    android.hardware.usb.gadget-service.qti \
+    usb_compositions.conf
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
