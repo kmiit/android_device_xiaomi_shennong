e32593375db59945aebe27588a68b156fae4a4ab
Cosmin Tanislav <demonsingur@gmail.com>
2025-01-07 22:20:37 +0200

    shennong: audio: Use soong_config_set macro
    Change-Id: Ic660ec8d67e323d89b6f82c2189428b66ca8d3a2

--- device.mk
+++ device.mk
@@ -53,10 +53,7 @@ PRODUCT_PACKAGES += \
     compat_framework_overrides.xml
 
 # Audio
-SOONG_CONFIG_NAMESPACES += android_hardware_audio
-SOONG_CONFIG_android_hardware_audio += \
-    run_64bit
-SOONG_CONFIG_android_hardware_audio_run_64bit := true
+$(call soong_config_set,android_hardware_audio,run_64bit,true)
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
