a6971aacf4bd70b3fe279f5759383ccaf8a75411
kmiit <kmiit@foxmail.com>
2025-01-10 14:20:32 +0800

    shennong: vendor: Update blobs to OS2.0.101.0.VNBCNXM

--- lineage_shennong.mk
+++ lineage_shennong.mk
@@ -21,9 +21,9 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_NAME := lineage_shennong
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc=$(call normalize-path-list, "shennong-user 15 AQ3A.240627.003 OS2.0.6.0.VNBCNXM release-keys")
+    BuildDesc=$(call normalize-path-list, "shennong-user 15 AQ3A.240627.003 OS2.0.101.0.VNBCNXM release-keys")
 
-BUILD_FINGERPRINT := Xiaomi/shennong/shennong:15/AQ3A.240627.003/OS2.0.6.0.VNBCNXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/shennong/shennong:15/AQ3A.240627.003/OS2.0.101.0.VNBCNXM:user/release-keys
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
