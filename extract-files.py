0bcd8c46ed6f5c97a75228383c6e2fdd27e184dc
Kmiit <kmiit@foxmail.com>
2025-02-06 11:10:40 +0000

    shennong: blobs: Partially update android.hardware.graphics.composer3-V2-ndk to V3

--- extract-files.py
+++ extract-files.py
@@ -189,6 +189,12 @@ blob_fixups: blob_fixups_user_type = {
         'odm/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so'
     ): blob_fixup()
         .replace_needed('android.hardware.graphics.allocator-V1-ndk.so', 'android.hardware.graphics.allocator-V2-ndk.so'),
+    (
+        'odm/bin/hw/vendor.xiaomi.sensor.citsensorservice.aidl',
+        # 'vendor/bin/hw/vendor.qti.hardware.display.composer-service',
+        'vendor/lib64/libdisplaydebug.so'
+    ): blob_fixup()
+        .replace_needed('android.hardware.graphics.composer3-V2-ndk.so', 'android.hardware.graphics.composer3-V3-ndk.so'),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
