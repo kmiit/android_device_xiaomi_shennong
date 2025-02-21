1925e92cf18ffb14804a14145b7219f406f8486a
kmiit <kmiit@foxmail.com>
2025-02-21 21:00:00 +0800

    shennong: sensors: Build sensors.dynamic_sensor_hal from source

--- device.mk
+++ device.mk
@@ -299,7 +299,8 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.xiaomi-multihal
+    android.hardware.sensors-service.xiaomi-multihal \
+    sensors.dynamic_sensor_hal
 
 PRODUCT_PACKAGES += \
     android.hardware.sensor.accelerometer.prebuilt.xml \
