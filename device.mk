c56abb5262a148d257222e2e370054160d15f0e2
Cosmin Tanislav <demonsingur@gmail.com>
2025-03-03 12:04:18 +0200

    shennong: vibrator: Move vibrator effect stream config to select
    Change-Id: I0878e645d12474da2d1971ce459db43c09bc97b7

--- device.mk
+++ device.mk
@@ -402,6 +402,8 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
+$(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.xiaomi)
+$(call soong_config_set,qti_vibrator,use_effect_stream,true)
 
 # WiFi
 PRODUCT_PACKAGES += \
