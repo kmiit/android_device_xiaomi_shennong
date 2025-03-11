d4747bc5f189a860a87f442996c3835c1f0d47c9
kmiit <kmiit@foxmail.com>
2025-03-12 03:06:42 +0800

    shennong: health: Switch to qcom common health-vendor-product.mk
    Signed-off-by: kmiit <kmiit@foxmail.com>

--- device.mk
+++ device.mk
@@ -175,9 +175,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml
 
 # Health
-PRODUCT_PACKAGES += \
-    android.hardware.health-service.qti \
-    android.hardware.health-service.qti_recovery
+$(call inherit-product, vendor/qcom/opensource/healthd-ext/health-vendor-product.mk)
 
 # IPACM
 PRODUCT_PACKAGES += \
