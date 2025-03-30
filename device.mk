f14c8ef188bb09f022060f9a935d3a9f496455ce
Kmiit <kmiit@foxmail.com>
2025-03-30 03:17:47 +0000

    shennong: sensors: Use aosp sensors service multihal

--- device.mk
+++ device.mk
@@ -297,7 +297,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors-service.xiaomi-multihal \
+    android.hardware.sensors-service.multihal \
     sensors.dynamic_sensor_hal
 
 PRODUCT_PACKAGES += \
