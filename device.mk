06135166a01a4f0ecb0c0c8cee2cee3bbd5b2b92
Bruno Martins <bgcngm@gmail.com>
2025-04-24 21:36:24 +0100

    shennong: Switch to PowerShare AIDL
    Change-Id: I97400638493a3b9d5a3b87fa6831c9affc7c3def

--- device.mk
+++ device.mk
@@ -277,7 +277,7 @@ PRODUCT_COPY_FILES += \
 # PowerShare
 $(call soong_config_set,lineage_powershare,powershare_path,/sys/class/qcom-battery/reverse_chg_mode)
 PRODUCT_PACKAGES += \
-    vendor.lineage.powershare@1.0-service.default
+    vendor.lineage.powershare-service.default
 
 # QSPA
 PRODUCT_PACKAGES += \
