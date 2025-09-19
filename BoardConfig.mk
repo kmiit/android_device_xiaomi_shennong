41ffda422c96d2e4fc499ac76a4ed8eb0bbd0be2
kmiit <kmiit@foxmail.com>
2025-09-19 13:44:39 +0000

    shennong: sepolicy: Update sepolicy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -191,7 +191,6 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Verified Boot
