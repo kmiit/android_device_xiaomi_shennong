f6051cee791bebc63e190f92229782d5328da972
kmiit <kmiit@foxmail.com>
2025-09-19 13:17:55 +0000

    shennong: Drop useles TARGET_RECOVERY_DEVICE_MODULES

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -69,7 +69,6 @@ BOARD_PREBUILT_DTBOIMAGE := $(KERNEL_PATH)/dtbo.img
 TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config/config.fs
 
 # Init
-TARGET_RECOVERY_DEVICE_MODULES := libinit_shennong
 $(call soong_config_set,libinit,vendor_init_lib,//$(DEVICE_PATH):libinit_shennong)
 
 # Kernel
