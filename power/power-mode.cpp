a2c941a921f19c4a54d7aa162b1600020b39846d
kmiit <kmiit@foxmail.com>
2025-10-11 04:31:54 +0000

    shennong: power: Adjust power-mode.cpp to new touch driver
    Signed-off-by: kmiit <kmiit@foxmail.com>

--- power/power-mode.cpp
+++ power/power-mode.cpp
@@ -9,12 +9,28 @@
 #include <android-base/logging.h>
 #include <sys/ioctl.h>
 
+#define COMMON_DATA_CMD 0
+#define SELECT_TOUCH_ID 3
+
 #define SET_CUR_VALUE 0
 #define TOUCH_DOUBLETAP_MODE 14
-#define TOUCH_MAGIC 't'
-#define TOUCH_IOC_SETMODE _IO(TOUCH_MAGIC, SET_CUR_VALUE)
+#define TOUCH_MAGIC 'T'
 #define TOUCH_DEV_PATH "/dev/xiaomi-touch"
 #define TOUCH_ID 0
+#define CMD_DATA_BUF_SIZE 128
+
+namespace {
+typedef struct {
+	int8_t touch_id = TOUCH_ID;
+	uint8_t cmd = SET_CUR_VALUE;
+	uint16_t mode;
+	uint16_t data_len = CMD_DATA_BUF_SIZE;
+	int32_t data_buf[CMD_DATA_BUF_SIZE];
+} common_data_t;
+}
+
+#define XIAOMI_IOC_COMMON_DATA _IOW(TOUCH_MAGIC, COMMON_DATA_CMD, common_data_t)
+#define XIAOMI_IOC_SELECT_TOUCH_ID _IOW(TOUCH_MAGIC, SELECT_TOUCH_ID, unsigned long)
 
 namespace aidl {
 namespace android {
@@ -38,8 +54,13 @@ bool setDeviceSpecificMode(Mode type, bool enabled) {
     switch (type) {
         case Mode::DOUBLE_TAP_TO_WAKE: {
             int fd = open(TOUCH_DEV_PATH, O_RDWR);
-            int arg[3] = {TOUCH_ID, TOUCH_DOUBLETAP_MODE, enabled ? 1 : 0};
-            ioctl(fd, TOUCH_IOC_SETMODE, &arg);
+            common_data_t data = {
+                .mode = TOUCH_DOUBLETAP_MODE,
+                .data_buf = {enabled ? 1 : 0},
+            };
+            // Select touch id in this fd first
+            ioctl(fd, XIAOMI_IOC_SELECT_TOUCH_ID, TOUCH_ID);
+            ioctl(fd, XIAOMI_IOC_COMMON_DATA, &data);
             close(fd);
             return true;
         }
