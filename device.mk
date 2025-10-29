870d2a22fe303addbffde6d8bf461dbf5c3bca99
kmiit <kmiit@foxmail.com>
2025-10-29 05:57:22 +0000

    shennong: audio: Build missing android.hardware.bluetooth.audio-impl
    Signed-off-by: kmiit <kmiit@foxmail.com>

--- device.mk
+++ device.mk
@@ -62,6 +62,7 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@7.1-impl \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
+    android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3-impl
 
 PRODUCT_PACKAGES += \
