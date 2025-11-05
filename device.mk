938201fee23e014c577bd935323ad4602b16eee1
kmiit <kmiit@foxmail.com>
2025-11-05 15:06:59 +0000

    shennong: parts: Drop XiaomiParts
    Signed-off-by: kmiit <kmiit@foxmail.com>

--- device.mk
+++ device.mk
@@ -267,10 +267,6 @@ PRODUCT_PACKAGES += \
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-# Parts
-PRODUCT_PACKAGES += \
-    XiaomiParts
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
